0e723d25cd87007a79385bc22a77f17cc750c401
Tenorio Castilla Carlos Yael <carlos.tenorio.14.81.33@gmail.com>
2022-09-03 23:11:12 -0500

    Actualización 03092022 11:11 pm

--- Niomina.java
+++ Niomina.java
@@ -6,7 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
+import java.util.ArrayList;
 
 /**
  *
@@ -15,72 +15,59 @@ import java.util.Arrays;
 public class Niomina {
 
     public static void main(String[] args) throws FileNotFoundException {
-        File Arch = new File("C:/Users/yael_/OneDrive/Escritorio/julio.dat"); //Sirve con "julio.dat" pero no con "junio.dat"??????
+        File Arch = new File("C:/Users/yael_/OneDrive/Escritorio/julio.dat"); //Sirve con "julio.dat" pero no con "junio.dat" POR??????
         Niomina Prueba = new Niomina(Arch);
+        Prueba.Lista_Sueldos();
     }
     int rozmiar = 0;
     int TamLis;
     String LineasL = "";
-    String Cortadora1 = "";
+    ArrayList<ArrayList<String>> OutterArray = new ArrayList();
+    Arreglos_ADT Nomina;
+    Empleado VerEmp;
     String[] Final;
     String[] Lineas;
     String[] Tempo;
-    Empleado Ya;
+    Empleado Emp;
 
-    public Niomina(File Arch) {
+    public Niomina(File Arch) { //Constructor
         try {
             Path Ruta = Arch.toPath();
             BufferedReader lector = Files.newBufferedReader(Ruta);
             String Leido;
 
             while ((Leido = lector.readLine()) != null) {
-                Lineas = Leido.split("\n");
+                Lineas = Leido.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
                 rozmiar += Lineas.length;
-                LineasL += Arrays.toString(Lineas);
-                //System.out.println(Lineas[0]);
+                ArrayList<String> InnerArray = new ArrayList();
+                for (String datos : Lineas) {
+                    InnerArray.add(datos);
+                }
+                OutterArray.add(InnerArray);
             }
-            
-            for (int Corr = 0; Corr < rozmiar; Corr++){
-                Final = LineasL.split(",");
-                Cortadora1 += Arrays.toString(Final);
-            }
-            System.out.println(Cortadora1);
-            
-            Arreglos_ADT Infierno = new Arreglos_ADT(rozmiar);
-            for (int Cor = 0; Cor < rozmiar; Cor++) {
-                Infierno.setItem(Cor, LineasL);
-                //System.out.println(Infierno.getItem(Cor));
-            }
-
 
             //Esta parte es para asignar los valores al ADT Empleado
-            /*Arreglos_ADT Lista_plac = new Arreglos_ADT(rozmiar - 1); //Constructor del arreglo.
-            for (int Indi = 0; Indi < rozmiar - 1; Indi++) {
-                Tempo = Arrays.toString(Lineas).split(",");
-                String Cadena = Arrays.toString(Tempo);
-                /*Empleado Ya = new Empleado(int (Cadena.charAt(0)
-                ), Cadena.charAt(1)
-                , Cadena.charAt(2)
-                , Cadena.charAt(3), int (Cadena.charAt(4)), int (Cadena.charAt(5)), int (Cadena.charAt(6)
-                ));
-                Lista_plac.setItem(Ya, Indi);
+            Nomina = new Arreglos_ADT(rozmiar);
+
+            for (int Filas = 1; Filas < OutterArray.size(); Filas++) {
+                for (int Columnas = 0; Columnas < OutterArray.size(); Columnas++) {
+                    Emp = new Empleado(Integer.parseInt(OutterArray.get(Filas).get(0)), OutterArray.get(Filas).get(1), OutterArray.get(Filas).get(2), OutterArray.get(Filas).get(3), Integer.parseInt(OutterArray.get(Filas).get(4)), Integer.parseInt(OutterArray.get(Filas).get(5)), Integer.parseInt(OutterArray.get(Filas).get(6)));
+                    Nomina.setItem(Columnas, Emp);
+                }
             }
-            TamLis = Lista_plac.getLenght();
-            Empleado No = new Empleado(5, "Cosa", "Casa", "Mort", 1, 23000, 1666);
-            System.out.print(No.to_String());*/
         } catch (IOException ex) {
         }
 
     }
 
-    /*public String Lista_Sueldos() {
-        for (int rozmiarS = 0; rozmiarS < TamLis; rozmiarS++) {
-            Emp = Ya.getItem(rozmiarS);
-            System.out.println("Id: " + Emp.get_NumTrabajador() + ", Sueldo: " + Emp.CalcularSueldo);
+    public void Lista_Sueldos() {
+        for (int rozmiarS = 0; rozmiarS < Nomina.getLenght(); rozmiarS++) {
+            CEmp = Nomina.getItem(rozmiarS);
+            System.out.println("Id: " + CEmp.get_NumTrabajador() + ", Sueldo: " + CEmp.CalcularSueldo());
         }
-    }*/
+    }
 
- /*public String Mayor_Ant() {
+    /*public String Mayor_Ant() {
         int Max = 2023;
         int Indice = 0;
         for (int ind = 0; ind < TamLis; ind++) {
